86d5b96d246f9401fee0f43affcd76a4c11ed13a
xavier-lai <xavier.lai@c-ways.com>
2023-06-22 09:58:26 +0200

    better swapping on bubble sort

--- src/sort/bubble_sort.py
+++ src/sort/bubble_sort.py
@@ -7,13 +7,12 @@ from ..decorators import time_it
 def bubble_sort(any_list: List[Any]) -> List[Any]:
     length = len(any_list)
 
-    at_least_on_swap_occured = False
+    at_least_one_swap_occured = False
     for iteration_count in range(length - 1):
         for index in range(length - 1 - iteration_count):
             if any_list[index] > any_list[index + 1]:
-                tmp_current_value = any_list[index]
-                any_list[index], any_list[index + 1] = any_list[index + 1], tmp_current_value
-                at_least_on_swap_occured = True
-        if at_least_on_swap_occured is False:
+                any_list[index], any_list[index + 1] = any_list[index + 1], any_list[index]
+                at_least_one_swap_occured = True
+        if at_least_one_swap_occured is False:
             return any_list
     return any_list
